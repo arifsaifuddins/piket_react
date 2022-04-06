8355e4afb4850d2cfb87d6f80d19d4ce23625f69
saifuddien <ariefsaifuddien01@gmail.com>
2022-04-06 17:43:18 +0200

    shalat

--- components/Shalat.jsx
+++ components/Shalat.jsx
@@ -8,6 +8,8 @@ export default function Shalat() {
   const [kota, setKota] = useState('Khartoum')
   const [negara, setNegara] = useState('Sudan')
   const [jadwal, setJadwal] = useState([])
+  const kot = document.querySelector('.kota')
+  const neg = document.querySelector('.neg')
 
   useEffect(() => { fetchData() }, [])
 
@@ -15,9 +17,19 @@ export default function Shalat() {
     return await fetch(`https://api.aladhan.com/v1/timingsByCity?city=${kota}&country=${negara}`)
       .then(res => res.json())
       .then(res => {
-        setJadwal(res.data.timings)
-        document.querySelector('.kota').value = ''
-        document.querySelector('.neg').value = ''
+        if (res.code == '200') {
+          setJadwal(res.data.timings)
+          kot.value = ''
+          neg.value = ''
+        } else if (res.code == '400') {
+          kot.value = ''
+          neg.value = ''
+          setKota('Khartoum')
+          setNegara('Sudan')
+          alert('Kota atau Negara tidak ada!, Coba lain')
+        } else {
+          setJadwal(res.data.timings)
+        }
       })
   }
 
@@ -35,7 +47,7 @@ export default function Shalat() {
         </div>
         <div className="mt-10 w-full flex">
           <input type="text" className='p-3 text-xl outline-none focus:border-[#f59800] border w-[50%] mr-2 shadow rounded-lg border-slate-800 kota' placeholder='Kota...' required onChange={(e) => setKota(e.target.value)} />
-          <input type="text" className='p-3 text-xl outline-none focus:border-[#f59800] border w-[50%] ml-2 shadow rounded-lg border-slate-800 neg' placeholder='Neagara...' required onChange={(e) => setNegara(e.target.value)} />
+          <input type="text" className='p-3 text-xl outline-none focus:border-[#f59800] border w-[50%] ml-2 shadow rounded-lg border-slate-800 neg' placeholder='Negara...' required onChange={(e) => setNegara(e.target.value)} />
         </div>
         <div className="mt-4 bg-[#f59800] hover:bg-[#f58500] p-3 shadow rounded-lg text-center font-bold text-2xl text-white cursor-pointer" onClick={() => fetchData()}>Ubah Lokasi</div>
       </div>
