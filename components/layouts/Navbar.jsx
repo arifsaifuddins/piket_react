12b946d37b617a3f3d1d18e8fbb90cb9ef2da70b
saifuddien <ariefsaifuddien01@gmail.com>
2022-04-06 06:42:18 +0200

    lp

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -10,22 +10,22 @@ export default function Navbar() {
     }
   }
   return (
-    <div className='shadow flex justify-around items-end py-4 bg-white text-[#f59800] fixed bottom-0 right-0 left-0 sm:w-[400px] mx-auto w-full'>
+    <div className='shadow flex justify-around items-end py-3 bg-white text-[#f59800] fixed bottom-0 right-0 left-0 sm:w-[400px] mx-auto w-full'>
       <NavLink to={'/menu'} style={active} className='flex flex-col items-center'>
         <img src='../svg/navbar/menu.svg' alt="menu" className='w-6 h-6' />
-        <h3 className='text-sm'>Menu</h3>
+        <h3 className='text-sm mt-1'>Menu</h3>
       </NavLink>
       <NavLink to={'/anggota'} style={active} className='flex flex-col items-center'>
         <img src='../svg/navbar/users.svg' alt="anggota" className='w-6 h-6' />
-        <h3 className='text-sm'>Anggota</h3>
+        <h3 className='text-sm mt-1'>Anggota</h3>
       </NavLink>
       <NavLink to={'/shalat'} style={active} className='flex flex-col items-center'>
         <img src='../svg/navbar/shalat.svg' alt="anggota" className='w-6 h-6' />
-        <h3 className='text-sm'>Shalat</h3>
+        <h3 className='text-sm mt-1'>Shalat</h3>
       </NavLink>
       <NavLink to={'/about'} style={active} className='flex flex-col items-center'>
         <img src='../svg/navbar/about.svg' alt="about" className='w-6 h-6' />
-        <h3 className='text-sm'>Tentang</h3>
+        <h3 className='text-sm mt-1'>Tentang</h3>
       </NavLink>
     </div>
   )
