a7f78bcf8b1cc5163f91821402606951f4f89ba9
saifuddien <ariefsaifuddien01@gmail.com>
2022-04-07 01:01:43 +0200

    header

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className='flex items-center fixed top-0 right-0 left-0 h-20 shadow bg-white justify-center sm:w-[400px] mx-auto w-full'>
       <img src="../icons/icon.png" alt="icon" className='w-14' />
-      <h1 className='text-3xl font-bold mx-3'>Piket <span className='bg-[#f59800] px-2 rounded-md text-white shadow-sm'>Sabtu</span></h1>
+      <h1 className='text-3xl font-bold mx-3'>Piket <span className='bg-[#f59800] p-1 px-3 rounded-md text-white shadow-sm'>Sabtu</span></h1>
     </div>
   )
 }
